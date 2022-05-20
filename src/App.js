434596a8e33ce79c6f10abdbf26561046e92a548
foadtkf <fakhrulif@gmail.com>
2022-05-20 20:42:24 +0600

    signup implemented

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Login from './Pages/Login/Login';
 import Appointment from './Pages/Appointment/Appointment';
+import Signup from './Pages/Login/Signup';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="about" element={<About />} />
         <Route path="login" element={<Login />} />
         <Route path='/appointment' element={<Appointment/>}/>
+        <Route path='/signup' element={<Signup/>}></Route>
       </Routes>
     </div>
   );
