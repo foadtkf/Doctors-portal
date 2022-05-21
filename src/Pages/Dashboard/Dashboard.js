93bda15cb8a7734be00e706e1228fd4114f4f06d
foadtkf <fakhrulif@gmail.com>
2022-05-21 15:57:01 +0600

    appointment list shown

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -4,13 +4,13 @@ import { Link, Outlet } from "react-router-dom";
 const Dashboard = () => {
   return (
     <div class="drawer drawer-mobile">
-      <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+      <input id="dashboard-sidebar" type="checkbox" class="drawer-toggle" />
       <div class="drawer-content flex flex-col items-center justify-center">
-      <h2 className='text-2xl font-bold text-purple-500'>Welcome to your Dashboard</h2>
+      <h1 className="text-6xl">This is your dashboard</h1>
         <Outlet />
       </div>
       <div class="drawer-side">
-        <label for="my-drawer-2" class="drawer-overlay"></label>
+        <label for="dashboard-sidebar" class="drawer-overlay"></label>
         <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
           <li>
             <Link to="/dashboard">My Appointments</Link>
